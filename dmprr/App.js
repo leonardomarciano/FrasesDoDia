18a93b7edd8535cb915216c16ebecbc1aba554de
LStark <djmars48@gmail.com>
2018-06-16 17:13:30 -0300

    Prey for the sorrow

--- dmprr/App.js
+++ dmprr/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
 
 export default class App extends React.Component {
 
   render() {
 
+
+    const numeroAleatorio = () => {
+      let numero = Math.random()
+      numero = Math.floor(numero * 5)
+      let frases = Array
+      frases[0] = 'Se hoje fosse o último dia de minha vida, queria fazer o que vou fazer hoje? E se a resposta fosse "Não" muitos dias seguidos, sabia que precisava mudar algo.'
+      frases[1] = 'Não faz sentido olhar para trás e pensar: devia ter feito isso ou aquilo, devia ter estado lá. Isso não importa. Vamos inventar o amanhã, e parar de nos preocupar com o passado.'
+      frases[2] = 'Cada sonho que você deixa pra trás, é um pedaço do seu futuro que deixa de existir.'
+      frases[3] = 'As pessoas não sabem o que querem, até mostrarmos a elas.'
+      frases[4] = 'Foco é dizer não.'
+      
+
+
+      alert( frases[numero])
+    }
+
     return (
       <View style={styles.container}>
-      
+        <Image source={require('./assets/logo.png')} />
+        <TouchableOpacity style={styles.botao} onPress={numeroAleatorio}> 
+        
+        <Text style={styles.textBotao}>Nova Frase</Text>
+        
+        </TouchableOpacity>
       </View>
     );
   }
@@ -16,8 +37,21 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  botao:{
+    marginTop: '10%',
+    backgroundColor: '#538539',
+    borderRadius: 3,
+    elevation: 2,
+    paddingVertical: 10, // x
+    paddingHorizontal: 40, // y
+  },
+  textBotao:{
+    color: 'white',
+    alignSelf: 'center',
+  }
 });
